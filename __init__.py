771b2d918791a6ab0f242531b5b0ec423165adfe
Moritz Hesche <mo.hesche@gmail.com>
2020-12-11 21:34:32 +0100

    added init to framework360

---
+++ __init__.py
@@ -0,0 +1,5 @@
+# the import here are used for direct importing the classes
+# if a class is import here you only need to call apper.<class>
+# instead of framework360.fusion_wrappers.<module.py>.<class>
+
+from .fusion_wrappers import FusionApp, Fusion360CommandBase, HandlerState
