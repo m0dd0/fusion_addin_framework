7cd89c68c582d0cb15c42977001a725e6a5f4258
Moritz Hesche <mo.hesche@gmail.com>
2020-12-11 21:50:25 +0100

    renamed enums and changed init

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 # the import here are used for direct importing the classes
-# if a class is import here you only need to call apper.<class>
+# if a class is import here you only need to call framework360.<class>
 # instead of framework360.fusion_wrappers.<module.py>.<class>
 
-from .fusion_wrappers import FusionApp, Fusion360CommandBase, HandlerState
+from .fusion_wrappers.fusion_app import FusionApp
+from .fusion_wrappers.fusion360_command_base import Fusion360CommandBase, HandlerState
+from .fusion_wrappers import ui_enums
--- fusion_wrappers/__init__.py
+++
@@ -1,6 +0,0 @@
-# the import here are used for direct importing the classes
-# if a class is import here you only need to call apper.<class>
-# instead of apper.<module.py>.<class>
-
-from .fusion_app import FusionApp
-from .fusion360_command_base import Fusion360CommandBase, HandlerState
