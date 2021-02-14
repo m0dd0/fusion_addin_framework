355e04806fde6a87bf3804b31ef7497adacc13ba
Moritz <mo.hesche@gmail.com>
2021-02-14 18:46:50 +0100

    updating init

--- fusion_addin_framework/__init__.py
+++ fusion_addin_framework/__init__.py
@@ -1,4 +1,4 @@
-from .src import wrapper, defaults, util, handlers, messages
+from . import wrapper, defaults, util, handlers, messages
 
-from .src.wrapper import FusionApp, Workspace, Tab, Panel, Button, Command
-from .src.util import py_utils
+from .wrapper import FusionApp, Workspace, Tab, Panel, Button, Command
+from .util import py_utils
