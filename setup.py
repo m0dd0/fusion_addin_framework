7aae4157b979ea667c8c3fde4df41622ec0ce77a
Moritz <mo.hesche@gmail.com>
2021-02-18 14:45:49 +0100

    manage versioning by scm_setuptools

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (Path(__file__).parent / "README.md").read_text()
 
 setuptools.setup(
     name="fusion_addin_framework",
-    version="0.0.1",
+    # version="0.0.1", # version is managed by setuptools_scm
     description="A framewotk to simplify the creation of Addins for Fusion360",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -26,9 +26,9 @@ setuptools.setup(
     },
     python_requires=">=3.7",  # current (01.2021) python version used by Fusion360
     install_requires=["appdirs"],
-    include_package_data=True
+    include_package_data=True, # needs to be kept even setuptools_scm is used !!!
     # including pacakge_data is managed automatically by setuptools_scm which is
     # also defined as buid_dependency in pyproject.toml
-    # use_scm_version=True,
-    # extras_require={"dev": ["build", "black", "pylint"]},
+    use_scm_version=True,
+    extras_require={"dev": ["build", "black", "pylint"]},
 )
