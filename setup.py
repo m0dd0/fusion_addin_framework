f87c7f3ba90b20d591ef4754ed51ec03479976ac
Moritz <mo.hesche@gmail.com>
2021-02-18 14:23:48 +0100

    working build setup

--- setup.py
+++ setup.py
@@ -3,27 +3,32 @@ from pathlib import Path
 
 README = (Path(__file__).parent / "README.md").read_text()
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
 setuptools.setup(
     name="fusion_addin_framework",
     version="0.0.1",
     description="A framewotk to simplify the creation of Addins for Fusion360",
-    long_description=long_description,
+    long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/m0dd0/fusion_addin_framework",
     author="Moritz Hesche",
     author_email="mo.hesche@gmail.com",
-    licence="MIT",
+    url="https://github.com/m0dd0/fusion_addin_framework",
+    packages=setuptools.find_packages(),
+    license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=setuptools.find_packages(),
-    python_requires=">=3.7",
-    install_package_data=True,
-    install_requires=[],
+    project_urls={
+        "Documentation": "https://fusion-addin-framework.readthedocs.io/en/stable/"
+    },
+    python_requires=">=3.7",  # current (01.2021) python version used by Fusion360
+    install_requires=["appdirs"],
+    include_package_data=True
+    # including pacakge_data is managed automatically by setuptools_scm which is
+    # also defined as buid_dependency in pyproject.toml
+    # use_scm_version=True,
+    # extras_require={"dev": ["build", "black", "pylint"]},
 )
