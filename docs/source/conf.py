01e4d88f7673dda759ebe969903de5c88cd318bb
Moritz <mo.hesche@gmail.com>
2021-08-03 10:05:09 +0200

    using contents.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -93,6 +93,7 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
 
+master_doc = "contents"
 
 autodoc_mock_imports = ["adsk"]
 
---
+++ docs/source/contents.rst
@@ -0,0 +1,8 @@
+.. toctree::
+   :hidden:
+
+   index
+   examples
+   reference
+   indices
+
