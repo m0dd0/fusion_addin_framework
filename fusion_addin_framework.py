ceaa262d91445f9403aecd4cefbbf95fb83c41ce
Moritz <mo.hesche@gmail.com>
2022-06-07 23:27:20 +0200

    improved format of test reports

--- fusion_addin_framework.py
+++ fusion_addin_framework.py
@@ -5,10 +5,10 @@ directory. In normal use this can be ignored and wont be recognized by Fuion360.
 """
 
 import traceback
-from pprint import pprint
 import logging
 from collections import defaultdict
 from time import perf_counter
+from typing import Dict, Union
 
 import adsk.core, adsk.fusion, adsk.cam
 
@@ -19,11 +19,37 @@ from . import fusion_addin_framework as faf
 # if a single testfunction is testes the addin.stop method is called at leaving addin
 # if multiple testfunctions are provided we stop the addin immideately after the test
 # which created it got executed.
-# TESTCASES = testcases.ALL_CASES
-TESTCASES = [testcases.test_thread_event_decorator]
+TESTCASES = testcases.ALL_CASES
+# TESTCASES = [testcases.test_thread_event_decorator]
 
 
 addin = None
+
+
+def format_results(result_dict: Dict[str, Dict[str, Union[str, float]]]) -> str:
+    """Gives a nice summarizing string of the results of the tests.
+
+    Args:
+        result_dict (Dict): The  dict containing all restults of the test mapped by their names.
+
+    Returns:
+        str: The formatted string.
+    """
+    # pprint(dict(results), width=200)
+    formatted_str = "### RESULTS ###"
+
+    formatted_str += f"\n{'test name':<50}\t{'elapsed time':<15}\tresult"
+    for test_name, test_result in result_dict.items():
+        formatted_str += f"\n{test_name:<50}"
+        formatted_str += f"\t{str(round(test_result['elapsed_time'],5)):<15}"
+        formatted_str += f"\t{'passed' if test_result['passed'] else 'failed'}"
+
+    if all([r["passed"] for r in result_dict.values()]):
+        formatted_str += "\n### PASSED ###"
+    else:
+        formatted_str += "\n### FAILED ###"
+
+    return formatted_str
 
 
 def run(context):  # pylint:disable=unused-argument
@@ -55,18 +81,15 @@ def run(context):  # pylint:disable=unused-argument
                 results[case.__name__]["passed"] = False
                 print(traceback.format_exc())
             try:
+                # if we only had one testcase we mostly want to analyse the result in fusion
+                # and therfore we shouldnt stop the addin directly after the test
                 if len(TESTCASES) > 1:
                     addin.stop()
                     addin = None
             except:
                 pass
 
-        print("### RESULTS ###")
-        pprint(dict(results), width=200)
-        if all([r["passed"] for r in results.values()]):
-            print("### PASSED ###")
-        else:
-            print("### FAILED ###")
+        print(format_results(results))
 
     except:
         if ui:
